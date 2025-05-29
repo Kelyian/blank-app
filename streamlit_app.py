0289d75940bdcf8984aca7eed025be9011aeb02a
Kelyian <Titus.sankei@strathmore.edu>
2025-05-29 09:38:59 +0300

    growing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-st.title(" My new app")
+st.title("Title : Bio data")
 st.write("This is a sample web app")
 
 first_name = st.text_input("First name")
 last_name = st.text_input("Last name")
-
-
+gender = st.selectbox("Gender", ["Male","Female"])
+age = st.number_input("Age", min_value=0, max_value=120, step=1)
+dob = st.date_input("Your date of birth")
+Marital_status = st.radio("Marital Status", ["Single", "Married", "Divorced", "Widowed"])
+Years_of_experience = st.slider("Years of Experience", 0, 50)
